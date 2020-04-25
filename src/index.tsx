58392263f973d7c9cc6439c19ce250666b2ce4fa
Elijah Watson <ElijahLWatson@Gmail.com>
2020-04-25 02:24:57 -0400

    Update api endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000'
+	uri: 'https://api.city-cards.org/graphql'
 });
 
 ReactDOM.render(
