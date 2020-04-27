d372a916088c524901930ea159ac37052963187a
Elijah Watson <ElijahLWatson@Gmail.com>
2020-04-27 01:35:11 -0400

    Update logo in site header

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css';
-
-import { Grid } from 'react-feather';
+import logo from '../../logo.png';
 
 export function Header() {
 	return (
 		<div className={styles.headerBar}>
 			<span className={styles.siteTitle}>
-				<Grid className={styles.siteIcon} />
+				<img src={logo} alt='logo' className={styles.siteLogo} />
 				<h1>City Cards</h1>
 			</span>
 		</div>
