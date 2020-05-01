99dca91e18cdcdd267eaa6b995bcb0727bae89c5
Elijah Watson <ElijahLWatson@Gmail.com>
2020-05-01 05:45:46 -0400

    Change GraphQL URI to env variable

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,12 @@ import store from './store';
 import { Provider } from 'react-redux';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const client = new ApolloClient({
-	uri: 'https://api.city-cards.org/graphql'
+	// eslint-disable-next-line no-undef
+	uri: process.env.REACT_APP_GRAPHQL_URI
 });
 
 ReactDOM.render(
